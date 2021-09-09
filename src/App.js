a9bf15b405947affd8aac8d19a570a3da3c09494
wonse <sinwonse@gmail.com>
2021-09-09 14:38:53 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { Dimensions, StatusBar } from 'react-native';
+import styled, { ThemeProvider } from 'styled-components';
+import IconButton from './components/IconButton';
+import Input from './components/Input';
+import { images } from './images';
+import { theme } from './theme';
+import Task from './components/Task';
+
+const Container = styled.SafeAreaView`
+    flex: 1;
+    background-color: ${({theme}) => theme.background};
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+const Title = styled.Text`
+    font-size: 40px;
+    font-weight: 600;
+    color: ${({theme}) => theme.main};
+    align-self: flex-start;
+    margin: 20px;
+`;
+
+const List = styled.ScrollView`
+    flex: 1;
+    width: ${({width}) => width - 40}px;
+`;
+
+export default function App() {
+    const width = Dimensions.get('window').width;
+    const [newTask, setNewTask] = useState('');
+    const [tasks, setTasks] = useState({
+        '1': { id: '1', text: 'Hanbit', completed: false},
+        '2': { id: '2', text: 'React Native', completed: true},
+        '3': { id: '3', text: 'React Native Sample', completed: false},
+        '4': { id: '4', text: 'Edit TODO Item', completed: false},
+    });
+
+    const _addTask = () => {
+        const ID = Date.now().toString();
+        const newTaskObject = {
+            [ID]: { id: ID, text: newTask, completed: false},
+        };
+        setNewTask('');
+        setTasks({...tasks, ...newTaskObject});
+    };
+
+    const _handleTextChange = text => {
+        setNewTask(text);
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor={theme.background}
+                />
+                <Title>TODO List</Title>
+                <Input
+                    placeholder="+ Add a Task"
+                    value={newTask}
+                    onChangeText={_handleTextChange}
+                    onSubmitEditing={_addTask}
+                />
+                <List width={width}>
+                    {Object.values(tasks)
+                        .reverse()
+                        .map(item => (
+                            <Task key={item.id} text={item.text} />
+                        ))}
+                </List>
+            </Container>
+        </ThemeProvider>
+    );
+}
---
+++ src/images.js
@@ -0,0 +1,11 @@
+import CheckCircleOutline from '../assets/icons/outline_circle_black.png';
+import CheckCircle from '../assets/icons/outline_check_circle_black.png';
+import DeleteForever from '../assets/icons/outline_delete_forever_black.png';
+import Edit from '../assets/icons/outline_edit_black.png';
+
+export const images = {
+    uncompleted: CheckCircleOutline,
+    completed: CheckCircle,
+    delete: DeleteForever,
+    update: Edit,
+};
